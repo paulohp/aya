bd19ce1199e51028153312b9c1d64dd4dc944ee4
Paulo Pires <taralloqueiroiz@gmail.com>
2014-06-27 08:19:29 -0300

    better appearance

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+// define globals
+var express  = require('express');
+var app      = express();
+var server   = require('http').Server(app);
+var io       = require('socket.io')(server);
+var request  = require('request');
+var fs       = require('fs');
+var _        = require('lodash');
+var cheerio  = require('cheerio');
+
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var url       = "https://drive.google.com/folderview?id=0B5pGhCQSPFB9bjZndkJLNkhnZG8&usp=sharing";
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.get('/', function (req, res) {
+  request.get(url, function(err, response, body){
+    if (err) throw err;
+
+    var $ = cheerio.load(body);
+    var result = {};
+    result.images = [];
+    $('.flip-entry-visual-card img').each(function(i, el){
+      result.images.push($(el).attr('src'));
+    });
+
+    res.render('index', {images: result.images});
+  })
+});
+
+
+server.listen(port, function(){
+  console.log('Application are running on port' + port);
+});
+
+
