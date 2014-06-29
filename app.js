5d5fe020a618b958f75f56c767c93edab16569d2
Paulo Pires <taralloqueiroiz@gmail.com>
2014-06-29 18:26:38 -0300

    added models route

--- app.js
+++ app.js
@@ -16,14 +16,23 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 app.get('/', function (req, res) {
-    res.render('index');
+  res.render('index');
+});
+
+app.get('/models', function (req, res) {
+  res.render('models');
+});
+
+app.get('/models/:id', function (req, res) {
+  res.render('models');
 });
 
 
 server.listen(port, ipaddress, function() {
-    console.log('%s: Node server started on %s:%d ...',
-                Date(Date.now() ), ipaddress, port);
+  console.log('%s: Node server started on %s:%d ...',
+    Date(Date.now() ), ipaddress, port);
 });
 
 
