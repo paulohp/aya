9dfb58e32d28d6e2074ca05a6513f180bcd503a3
Paulo Pires <taralloqueiroiz@gmail.com>
2014-06-29 17:51:04 -0300

    redesign

--- app.js
+++ app.js
@@ -10,26 +10,14 @@ var _        = require('lodash');
 var cheerio  = require('cheerio');
 
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var url       = "https://drive.google.com/folderview?id=0B5pGhCQSPFB9bjZndkJLNkhnZG8&usp=sharing";
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.get('/', function (req, res) {
-  request.get(url, function(err, response, body){
-    if (err) throw err;
-
-    var $ = cheerio.load(body);
-    var result = {};
-    result.images = [];
-    $('.flip-entry-visual-card img').each(function(i, el){
-      result.images.push($(el).attr('src'));
-    });
-
-    res.render('index', {images: result.images});
-  })
+    res.render('index');
 });
 
 
